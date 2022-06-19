12a230fb722dffad9ddac8e5393590fb03dcd828
tokku5552 <physics.0121@gmail.com>
2022-06-19 20:39:25 +0900

    Initial commit

---
+++ test/cdk-context-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkContextSample from '../lib/cdk-context-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-context-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkContextSample.CdkContextSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
